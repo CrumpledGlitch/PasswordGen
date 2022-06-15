91e1e7502f2e2d7e095605b6380a4624760f9f25
CrumpledGlitch <69207169+CrumpledGlitch@users.noreply.github.com>
2022-06-15 12:45:03 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+function getPassword() {
+    const chars= "1234567890-=!£$%^&*()_+`¬QWERTYUIOP{}[]ASDFGHJKL:@~;'#|\ZXCVBNM<>?,./qwertyuiopasdfghjklzxcvbnm"
+
+    let passwordLength= 16
+    let password = '';
+    for (let i=0; i< passwordLength; i++) {
+        let randomNumber =Math.floor(Math.random() * chars.length);
+        password+= chars.substring(randomNumber, randomNumber +1);
+        console.log(password)
+        }
+        document.getElementById("password").value=password;
+}
